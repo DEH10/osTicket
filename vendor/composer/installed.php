1510fb81584a453e65f78df5b5aa1d0a63e873dc
DEH <156702447+DEH10@users.noreply.github.com>
2024-06-21 19:59:33 +0000

    php mysql

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'osticket/osticket',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '56fd46fd79c77efff56fe82a269a6523ce69c6bb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'osticket/osticket' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '56fd46fd79c77efff56fe82a269a6523ce69c6bb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

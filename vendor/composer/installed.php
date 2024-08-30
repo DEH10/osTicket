d14368c367f272a2dbc2ccb3fa5248d0cd92830f
DEH <156702447+DEH10@users.noreply.github.com>
2024-08-30 17:47:17 +0000

    mysqli extention  comment-out removed ->  ;

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'osticket/osticket',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '1510fb81584a453e65f78df5b5aa1d0a63e873dc',
+        'reference' => '0132fe9a24c3b8b3eb2bae68845b18d4f6243227',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'osticket/osticket' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '1510fb81584a453e65f78df5b5aa1d0a63e873dc',
+            'reference' => '0132fe9a24c3b8b3eb2bae68845b18d4f6243227',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
